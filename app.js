add0c4d689df0f86b942e88f07bc1dd26c9aecf0
Emmanuel Alexei Ortiz Aldana <a01747297@itesm.mx>
2021-05-18 18:20:59 -0500

    Se realizaron cambios xd

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 /* Informacion obtenida de: https://www.youtube.com/watch?v=HiXLkL42tMU&list=LL&index=30&t=59s */
 
 console.log("Hola mundo")
+
+//git init: inicias el proyecto
+
+//git add <file>: añades un archivo a tu espacio de trabajo, tambien se aplica cuando
+//                 modificas un archivo y debes agregarlo de nuevo
+
+//git status: te devuelve el estatus actual del proyecto
+
+//git commit: The git commit command captures a snapshot of the project's currently 
+//            staged changes. Lo ponemos siempre que queramos guardar los ultimos cambios
+//            hechos al proyecto. 
+//            Presionas i para escribir hasta arriba y dar un mensaje, luego le das esc
+//            y finalmente sale con :wq
+
+//git checkout -- <file> : eliminas los ultimos cambios de ese archivo
+
+//git diff <file> : te muestra los ultimos cambios hechos al archivo
