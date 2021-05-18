e98ebab9fdfb316f04f70829cc523112aa93c64c
Emmanuel Alexei Ortiz Aldana <a01747297@itesm.mx>
2021-05-18 18:50:48 -0500

    nueva version ajio

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ console.log("Hola mundo")
 //git add <file>: añades un archivo a tu espacio de trabajo, tambien se aplica cuando
 //                 modificas un archivo y debes agregarlo de nuevo
 
+// git add . : asi agregas todos los archivos de golpe
+
 //git status: te devuelve el estatus actual del proyecto
 
 //git commit: The git commit command captures a snapshot of the project's currently 
@@ -15,6 +17,20 @@ console.log("Hola mundo")
 //            Presionas i para escribir hasta arriba y dar un mensaje, luego le das esc
 //            y finalmente sale con :wq
 
+/* git commit -m "Mensaje: De esta manera evitamos entrar al editor de codigo, pero
+                        es lo mismo que el de arriba" */
+
 //git checkout -- <file> : eliminas los ultimos cambios de ese archivo
 
 //git diff <file> : te muestra los ultimos cambios hechos al archivo
+
+//git log: te da el historial de cambios
+
+//En el archivo de texto .gitignore ponemos los nombres de todos los archivos que no queremos
+//agregar a nuestro entorno de trabajo
+
+// git branch: Nos dice el proyecto en que estamos trabajando y sus versiones alternativas
+
+// git branch <nombre>: Aqui creamos una version alternativa, para entrar a esta version
+//                 alternativa pones git checkout <nombre>. Aqui puedes modificar lo que
+//                 quieras sin alterar a la version original
